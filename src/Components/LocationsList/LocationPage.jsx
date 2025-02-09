ca696f8842d357c24b701a578b6ccdf1890f88ff
Ananddivine <109467220+Ananddivine@users.noreply.github.com>
2025-02-09 19:13:06 +0530

    Initial commit

--- src/Components/LocationsList/LocationPage.jsx
+++ src/Components/LocationsList/LocationPage.jsx
@@ -8,13 +8,23 @@ const LocationPage = () => {
 
   return (
     <div className="location-container">
-      <Helmet>
-        <title>Laptop Service Center near {formattedLocation} | Skylap</title>
-        <meta
-          name="description"
-          content={`Looking for a laptop service center in ${formattedLocation}? Skylap offers expert laptop repair, screen replacement, and more.`}
-        />
-      </Helmet>
+     <Helmet>
+  <title>Laptop Service Center near {formattedLocation} | Skylap</title>
+  <meta
+    name="description"
+    content={`Looking for a laptop service center in ${formattedLocation}? Skylap offers expert laptop repair, screen replacement, and more.`}
+  />
+  {/* Open Graph tags */}
+  <meta property="og:title" content={`Laptop Service Center near ${formattedLocation} | Skylap`} />
+  <meta
+    property="og:description"
+    content={`Looking for a laptop service center in ${formattedLocation}? Skylap offers expert laptop repair, screen replacement, and more.`}
+  />
+  <meta property="og:image" content={`https://skylap.in/images/default-logo.png`} />
+  <meta property="og:url" content={`https://skylap.in//laptop-service-center-in/${location}`} />
+  <meta property="og:type" content="website" />
+</Helmet>
+
       <h1>Laptop Service Center near{formattedLocation}</h1>
       <p>Welcome to Skylap's service center near {formattedLocation}. We provide expert laptop repair and maintenance services in your area.</p>
     </div>
